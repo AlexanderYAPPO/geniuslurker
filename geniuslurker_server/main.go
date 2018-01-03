f2fabd21872d55003c64a62bf5fdb39e98be6c8c
Alexander Aleksiyants <yappo@yandex-team.ru>
2018-01-03 16:30:22 +0500

    search handler added

--- geniuslurker_server/main.go
+++ geniuslurker_server/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
-	"io"
+	"github.com/yappo/geniuslurker/geniuslurker"
 	"log"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+func search(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	res := geniuslurker.GetSearchResults(vars["q"])
+	b, err := json.Marshal(res)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Failed to search genius", http.StatusInternalServerError)
+		return
+	}
+	w.Write(b)
 }
 
 func main() {
 	rtr := mux.NewRouter()
-	rtr.HandleFunc("/user/profile", hello).Methods("GET")
+	rtr.HandleFunc("/search", search).Methods("GET").Queries("q", "{q}")
 
 	http.Handle("/", rtr)
 
