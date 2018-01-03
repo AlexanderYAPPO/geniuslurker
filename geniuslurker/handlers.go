592185d5f5458b6dadd362ee64c2e6b0699b8509
Alexander Aleksiyants <yappo@yandex-team.ru>
2018-01-03 17:51:48 +0500

    het lyrics handler added

---
+++ geniuslurker/handlers.go
@@ -0,0 +1,43 @@
+package geniuslurker
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func fetchLyricsFromGenius(url string) string {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("whoops:", err)
+	}
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	lyrics := GetLyricsFromHTML(strings.NewReader(string(bodyBytes)))
+	return lyrics
+}
+
+func GetLyricsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	url := vars["url"]
+	lyrics := fetchLyricsFromGenius(url)
+	io.WriteString(w, lyrics)
+}
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	res := GetSearchResults(vars["q"])
+	b, err := json.Marshal(res)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Failed to search genius", http.StatusInternalServerError)
+		return
+	}
+	w.Write(b)
+}
--- geniuslurker_server/main.go
+++ geniuslurker_server/main.go
@@ -1,29 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/yappo/geniuslurker/geniuslurker"
 	"log"
 	"net/http"
 )
 
-func search(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	res := geniuslurker.GetSearchResults(vars["q"])
-	b, err := json.Marshal(res)
-	if err != nil {
-		fmt.Println(err)
-		http.Error(w, "Failed to search genius", http.StatusInternalServerError)
-		return
-	}
-	w.Write(b)
-}
-
 func main() {
 	rtr := mux.NewRouter()
-	rtr.HandleFunc("/search", search).Methods("GET").Queries("q", "{q}")
+	rtr.HandleFunc("/search", geniuslurker.SearchHandler).Methods("GET").Queries("q", "{q}")
+	rtr.HandleFunc("/lyrics", geniuslurker.GetLyricsHandler).Methods("GET").Queries("url", "{url}")
 
 	http.Handle("/", rtr)
 
